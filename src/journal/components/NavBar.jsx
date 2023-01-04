c3b7d82c500c789fc4086312d8eea9ac68925afa
Luis Mera <luismera86@gmail.com>
2023-01-03 23:52:01 -0300

    Journal App

--- src/journal/components/NavBar.jsx
+++ src/journal/components/NavBar.jsx
@@ -1,15 +1,15 @@
-import { AppBar, IconButton, Toolbar } from "@mui/material"
+import { AppBar, IconButton, Toolbar } from '@mui/material'
 
-import { MenuOutlined } from "@mui/icons-material"
+import { MenuOutlined } from '@mui/icons-material'
 
-export const NavBar = () => {
+export const NavBar = ({ drawerWidth }) => {
   return (
-    <AppBar position="fixed">
+    <AppBar position='fixed' sx={{ sm: `calc(100% - ${drawerWidth}px)`, md: `${drawerWidth}px`}}> {/* lo que hacemos es que en pantallas pequeñas small calcule el 100% del ancho de la pantalla menos el drawerWith que va a ocupar el silderbar y en pantallas medianas medium solo descuente el drawerWhit */}
       <Toolbar>
         <IconButton>
           <MenuOutlined />
         </IconButton>
       </Toolbar>
-  </AppBar>
-    )
+    </AppBar>
+  )
 }
--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,15 +1,19 @@
-import { Box } from "@mui/material"
+import { Box } from '@mui/material'
+import { NavBar } from '../components'
 
-export const JournalLayout = ({children}) => {
+const drawerWidth = 240
+
+export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex' }}>
       {/* Navbar */}
+      <NavBar drawerWidth={drawerWidth} />
       {/* SideBar */}
-      <Box component='main' sx={{flexGrow: 1, p: 3}}>
+      <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
         {/* Toolbar */}
 
         {children}
       </Box>
-  </Box>
-    )
+    </Box>
+  )
 }
